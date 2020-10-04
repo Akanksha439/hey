1ecc52eba030f56372e057b48240c0c4ca5cd3e2
akanksha <akanksha439@gmail.com>
2020-10-04 23:10:07 +0530

    link mongodb using mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/hey-development');
+
+const db=mongoose.connection;
+db.on('error',console.error.bind(console,"error in connecting to mongodb"));
+
+db.once('open',function(){
+    console.log('connected to database::mongodb');
+});
+
+module.exports=db;
