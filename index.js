05c01f910c9049583f894c8fb598aa72e59f56ac
akanksha <akanksha439@gmail.com>
2020-10-04 23:37:28 +0530

    set up static files

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 const express=require('express');
 const app=express();
 const port=8000;
-const expressLayouts=require('express-ejs-layouts');
 const db=require('./config/mongoose');
+const expressLayouts=require('express-ejs-layouts');
 
+//access static files
+app.use(express.static('./assets'));
 //use layout
 app.use(expressLayouts);
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 //use express router
 app.use('/',require('./routes'));
 
